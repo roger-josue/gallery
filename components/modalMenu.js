03240e6574a6d0b82493a2dc8b00e147d7345c9e
roger-josue <jovz1798@gmail.com>
2023-06-10 16:17:58 -0600

    Style and layout changes -Changed the footer to an actual footer tag, which was wrongly set as a div. -Removed download button on images when they are not zoomed-in, left it just on modal view. -Cursor-pointer styles for different states.

--- components/modalMenu.js
+++ components/modalMenu.js
@@ -6,9 +6,7 @@ import DownloadButton from "./DownloadButton";
 
 export default function ModalMenu({ toggleModal, setToggleModal, photo }) {
 
-    const closeOnclick = ["dialog"];
     const modal = useRef(false);
-    // const [viewportWith, setviewportWith] = useState(null);
 
     useEffect(() => {
         if (toggleModal) {
@@ -19,14 +17,14 @@ export default function ModalMenu({ toggleModal, setToggleModal, photo }) {
     }, [toggleModal, setToggleModal])
 
     const handleExit = ({ type, code, target }) => {
-        if (type === "keydown" && code === "Escape" || type === "click" && closeOnclick.includes(target.localName))
+        if (type === "keydown" && code === "Escape" || type === "click" && target.localName === "dialog")
             setToggleModal(false);
     }
 
 
     return (
-        <motion.dialog initial={{opacity: 0}} transition={{duration: 0.5}} whileInView={{opacity: 1}} viewport={{once: true}} onKeyDown={(e) => handleExit(e)} onClick={(e) => handleExit(e)} ref={modal} className="fixed z-20 inset-0 w-screen h-screen bg-gray-800 bg-opacity-70 flex flex-col place-content-center items-center p-20">
-            <motion.div transition={{ duration: 1 }} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="relative w-[90vw] h-[90vh] sm:w-[60vw] sm:h-[80vh] lg:w-[50vw] lg:h-[70vh]">
+        <motion.dialog initial={{opacity: 0}} transition={{duration: 0.5}} whileInView={{opacity: 1}} viewport={{once: true}} onKeyDown={(e) => handleExit(e)} onClick={(e) => handleExit(e)} ref={modal} className="fixed z-20 inset-0 w-screen h-screen bg-gray-800 bg-opacity-70 flex flex-col place-content-center items-center p-20 cursor-zoom-out">
+            <motion.div transition={{ duration: 1 }} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="relative w-[90vw] h-[90vh] sm:w-[60vw] sm:h-[80vh] lg:w-[50vw] lg:h-[70vh] cursor-default">
                 <Image priority fill src={`${photo.urls.regular}&fm=jpg&fit=max`} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOsnPi1HgAGCgKA0hWgaQAAAABJRU5ErkJggg==" alt={photo.user.username} className="object-cover opacity-95" />
                 <a
                     className="absolute text-lg bottom-0 left-2 transition-all duration-500 focus:text-primary hover:text-primary"
